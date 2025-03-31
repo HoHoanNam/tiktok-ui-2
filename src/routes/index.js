949bff052e2591484fa9714330f49a1d1e9dc136
HoanNam <m8mi0909@gmail.com>
2025-03-31 12:48:54 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layouts';
+
+// Pages
+import Home from '~/pages/Home';
+import Search from '~/pages/Search';
+import Upload from '~/pages/Upload';
+import Profile from '~/pages/Profile';
+import Following from '~/pages/Following';
+
+// Không cần đăng nhập vẫn vào được
+const publicRoutes = [
+  { path: '/', component: Home },
+  { path: '/profile', component: Profile },
+  { path: '/following', component: Following },
+  { path: '/search', component: Search, layout: null },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+// Phải đăng nhập mới vào được
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
