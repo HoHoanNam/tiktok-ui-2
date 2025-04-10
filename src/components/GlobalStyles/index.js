fd26d9fdfcd8565ea4c2404ff9cc371dfa95d924
HoanNam <m8mi0909@gmail.com>
2025-04-10 20:35:45 +0700

    Giải quyết vấn đề nhiều file index.js

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -1,15 +1,1 @@
-// import React from 'react';
-import PropTypes from 'prop-types';
-import './GlobalStyles.scss';
-
-// Chỉ nhận duy nhất 1 children
-function GlobalStyles({ children }) {
-  // return React.Children.only(children);
-  return children;
-}
-
-GlobalStyles.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-export default GlobalStyles;
+export { default } from './GlobalStyles';
---
+++ src/routes/routes.js
@@ -0,0 +1,24 @@
+// Layouts
+import config from '~/config';
+import { HeaderOnly } from '~/layouts';
+
+// Pages
+import Home from '~/pages/Home';
+import Search from '~/pages/Search';
+import Upload from '~/pages/Upload';
+import Profile from '~/pages/Profile';
+import Following from '~/pages/Following';
+
+// Không cần đăng nhập vẫn vào được
+const publicRoutes = [
+  { path: config.routes.home, component: Home },
+  { path: config.routes.profile, component: Profile },
+  { path: config.routes.following, component: Following },
+  { path: config.routes.search, component: Search, layout: null },
+  { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+];
+
+// Phải đăng nhập mới vào được
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
